f59f834d629dda2fa25d826eff7651a95df76df5
Arnout Engelen <arnouten@bzzt.net>
2017-10-06 22:04:57 +0200

    Correctly copy resources and user accounts

--- generate.sh
+++ generate.sh
@@ -10,9 +10,7 @@ export CODE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [[ -d $SOURCE ]] || exit "Please set SOURCE to the source git directory"
 [[ -d $TARGET ]] || exit "Please set TARGET to the target git directory"
 
-cd $TARGET
-cp -r $CODE/resources .
-pwd
-cp -r $SOURCE/users .
+cp -r $CODE/resources/* $TARGET
+cp -r $SOURCE/users $TARGET
 
 # And convert $SOURCE/pages/*.md to $TARGET/*.html :)
