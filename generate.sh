052404cb7ef5b64a80069a0e423062eb76e7b3b5
Arnout Engelen <arnouten@bzzt.net>
2017-10-06 21:49:53 +0200

    Some initial code

---
+++ generate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function fail {
+  echo $1
+  exit 1
+}
+
+export CODE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+[[ -d $SOURCE ]] || exit "Please set SOURCE to the source git directory"
+[[ -d $TARGET ]] || exit "Please set TARGET to the target git directory"
+
+cd $TARGET
+cp -r $CODE/resources .
+cp -r $SOURCE/users .
+
+# And convert $SOURCE/pages/*.md to $TARGET/*.html :)
